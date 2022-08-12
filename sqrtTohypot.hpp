e1be06f91fb969524cc3c3a48bd2331d2c541eb0
yanghongru666 <1158162970@qq.com>
2022-08-12 11:40:30 +0800

    sqrtTohypot

--- sqrtTohypot.hpp
+++ sqrtTohypot.hpp
@@ -6,29 +6,29 @@ std::unique_ptr<ExprAST> sqrtTohypot(std::unique_ptr<ExprAST> &expr)
     if (expr->type() == "Binary")
     {
         BinaryExprAST *binOp = dynamic_cast<BinaryExprAST *>(expr.get());
-        char op = binOp->getOp(); //主表达式的二元运算符
+        char op = binOp->getOp();
 
-        std::unique_ptr<ExprAST> &lhs = binOp->getLHS(); //左子表达式
-        std::unique_ptr<ExprAST> &rhs = binOp->getRHS(); //右子表达式
+        std::unique_ptr<ExprAST> &lhs = binOp->getLHS();
+        std::unique_ptr<ExprAST> &rhs = binOp->getRHS();
 
         const std::string exprTypeLHS = lhs->type();
         const std::string exprTypeRHS = rhs->type();
 
         if (op == '+')
         {
-            if ((exprTypeLHS == "Binary") && (exprTypeRHS == "Binary")) // 左子表达式和右子表达式都是二元运算符表达式,且主表达式的运算符为+
+            if ((exprTypeLHS == "Binary") && (exprTypeRHS == "Binary"))
             {
-                BinaryExprAST *binOpL = dynamic_cast<BinaryExprAST *>(lhs.get()); //左子表达式
-                BinaryExprAST *binOpR = dynamic_cast<BinaryExprAST *>(rhs.get()); //右子表达式
+                BinaryExprAST *binOpL = dynamic_cast<BinaryExprAST *>(lhs.get());
+                BinaryExprAST *binOpR = dynamic_cast<BinaryExprAST *>(rhs.get());
 
-                char opL = binOpL->getOp(); //左子表达式的运算符
-                char opR = binOpR->getOp(); //右子表达式的运算符
+                char opL = binOpL->getOp();
+                char opR = binOpR->getOp();
                 if ((opL == '*') && (opR == '*'))
                 {
-                    std::unique_ptr<ExprAST> &lhsL = binOpL->getLHS(); //左子表达式的左表达式
-                    std::unique_ptr<ExprAST> &rhsL = binOpL->getRHS(); //左子表达式的左表达式
-                    std::unique_ptr<ExprAST> &lhsR = binOpR->getLHS(); //右子表达式的左表达式
-                    std::unique_ptr<ExprAST> &rhsR = binOpR->getRHS(); //右子表达式的右表达式
+                    std::unique_ptr<ExprAST> &lhsL = binOpL->getLHS();
+                    std::unique_ptr<ExprAST> &rhsL = binOpL->getRHS();
+                    std::unique_ptr<ExprAST> &lhsR = binOpR->getLHS();
+                    std::unique_ptr<ExprAST> &rhsR = binOpR->getRHS();
                     vector<ExprAST> args;
                     if (isEqual(lhsL, rhsL) && isEqual(lhsR, rhsR))
                     {
