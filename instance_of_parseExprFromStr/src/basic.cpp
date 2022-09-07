368d1090dd22580ae28936ce7cc328f0cfd0f82e
liu__yi <liu__yi>
2022-09-07 21:10:10 +0800

    instance_of_parseExprFromStr/

---
+++ instance_of_parseExprFromStr/src/basic.cpp
@@ -0,0 +1,300 @@
+#include "basic.hpp"
+
+//===----------------------------------------------------------------------===//
+// basic operation
+//===----------------------------------------------------------------------===//
+
+bool isEqual(const std::unique_ptr<ExprAST> &expr1, const std::unique_ptr<ExprAST> &expr2)
+{
+    if(expr1 == nullptr || expr2 == nullptr)
+    {
+        fprintf(stderr, "empty\n");
+        return false;
+    }
+
+    const std::string expr1Type = expr1->type();
+    const std::string expr2Type = expr2->type();
+
+    if(expr1Type == expr2Type)
+    {
+        if(expr1->type() == "Number")
+        {
+            NumberExprAST *numberExpr1 = dynamic_cast<NumberExprAST *>(expr1.get());
+            double number1 = (numberExpr1->getNumber());
+            NumberExprAST *numberExpr2 = dynamic_cast<NumberExprAST *>(expr2.get());
+            double number2 = (numberExpr2->getNumber());
+
+            return (number1 == number2);
+        }
+        else if(expr1->type() == "Variable")
+        {
+            VariableExprAST *variableExpr1 = dynamic_cast<VariableExprAST *>(expr1.get());
+            std::string variable1 = (variableExpr1->getVariable());
+            VariableExprAST *variableExpr2 = dynamic_cast<VariableExprAST *>(expr2.get());
+            std::string variable2 = (variableExpr2->getVariable());
+
+            return (variable1 == variable2);
+        }
+        else if(expr1->type() == "Call")
+        {
+            CallExprAST *callExpr1 = dynamic_cast<CallExprAST *>(expr1.get());
+            std::string callee1 = (callExpr1->getCallee());
+            CallExprAST *callExpr2 = dynamic_cast<CallExprAST *>(expr2.get());
+            std::string callee2 = (callExpr2->getCallee());
+
+            if(callee1 == callee2)
+            {
+                std::vector<std::unique_ptr<ExprAST>> &args1 = callExpr1->getArgs();
+                std::vector<std::unique_ptr<ExprAST>> &args2 = callExpr2->getArgs();
+
+                if(args1.size() == args2.size())
+                {
+                    for(long unsigned int i = 0; i < args1.size(); ++i)
+                    {
+                        if(!isEqual(args1.at(i), args2.at(i)))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if(expr1Type == "Binary")
+        {
+            BinaryExprAST *binOp1 = dynamic_cast<BinaryExprAST *>(expr1.get());
+            char op1 = binOp1->getOp();
+            std::string opStr1(1, op1);
+            BinaryExprAST *binOp2 = dynamic_cast<BinaryExprAST *>(expr2.get());
+            char op2 = binOp2->getOp();
+            std::string opStr2(1, op2);
+            if(opStr1 == opStr2)
+            {
+                std::unique_ptr<ExprAST> &lhs1 = binOp1->getLHS();
+                std::unique_ptr<ExprAST> &rhs1 = binOp1->getRHS();
+                std::unique_ptr<ExprAST> &lhs2 = binOp2->getLHS();
+                std::unique_ptr<ExprAST> &rhs2 = binOp2->getRHS();
+
+                return (isEqual(lhs1, lhs2) && isEqual(rhs1, rhs2));
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;  // TODO: handle the other cases
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool isFraction(const std::unique_ptr<ExprAST> &expr)
+{
+    if (expr == nullptr)
+    {
+        fprintf(stderr, "\tERROR: isFraction: the input expr is nullptr!\n");
+        return false;
+    }
+    if (expr->type() == "Binary")
+    {
+        BinaryExprAST *binOpPtr = dynamic_cast<BinaryExprAST *>(expr.get());
+        char op = binOpPtr->getOp();
+        if (op == '/')
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::unique_ptr<ExprAST> getNumerator(const std::unique_ptr<ExprAST> &expr)
+{
+    if (expr == nullptr)
+    {
+        fprintf(stderr, "\tERROR: getNumerator: the input expr is nullptr!\n");
+        return nullptr;
+    }
+    if (expr->type() == "Binary")
+    {
+        BinaryExprAST *binOpPtr = dynamic_cast<BinaryExprAST *>(expr.get());
+        char op = binOpPtr->getOp();
+        if (op == '/')
+        {
+            return std::move(binOpPtr->getLHS());
+        }
+    }
+    fprintf(stderr, "\tWARNING: getNumerator: the input expr is not match for fraction\n");
+    return expr->Clone();
+}
+
+std::unique_ptr<ExprAST> getDenominator(const std::unique_ptr<ExprAST> &expr)
+{
+    if (expr == nullptr)
+    {
+        fprintf(stderr, "\tERROR: getDenominator: the input expr is nullptr!\n");
+        return nullptr;
+    }
+    if (expr->type() == "Binary")
+    {
+        BinaryExprAST *binOpPtr = dynamic_cast<BinaryExprAST *>(expr.get());
+        char op = binOpPtr->getOp();
+        if (op == '/')
+        {
+            return std::move(binOpPtr->getRHS());
+        }
+    }
+    fprintf(stderr, "\tWARNING: getDenominator: the input expr is not match for fraction\n");
+    return expr->Clone();
+}
+
+std::unique_ptr<ExprAST> createBinaryExpr(const std::unique_ptr<ExprAST> &expr1, const std::unique_ptr<ExprAST> &expr2, const char op)
+{
+    if (expr1 == nullptr || expr2 == nullptr)
+    {
+        fprintf(stderr, "\tERROR: createBinaryExpr: the input contain nullptr!");
+        return nullptr;
+    }
+
+    auto lhs = expr1->Clone();
+    auto rhs = expr2->Clone();
+    return std::make_unique<BinaryExprAST>(op, std::move(lhs), std::move(rhs));
+}
+
+std::unique_ptr<ExprAST> addExpr(const std::unique_ptr<ExprAST> &expr1, const std::unique_ptr<ExprAST> &expr2) { return createBinaryExpr(expr1, expr2, '+'); }
+
+std::unique_ptr<ExprAST> mulExpr(const std::unique_ptr<ExprAST> &expr1, const std::unique_ptr<ExprAST> &expr2) { return createBinaryExpr(expr1, expr2, '*'); }
+
+std::unique_ptr<ExprAST> divExpr(const std::unique_ptr<ExprAST> &expr1, const std::unique_ptr<ExprAST> &expr2) { return createBinaryExpr(expr1, expr2, '/'); }
+
+//===----------------------------------------------------------------------===//
+// print information
+//===----------------------------------------------------------------------===//
+
+std::string PrintExpression(std::unique_ptr<ExprAST> &expr)
+{
+    if(expr == nullptr)
+    {
+        fprintf(stderr, "this is a nullptr.\n");
+    }
+// const std::string exprType = expr->type();
+#ifdef DEBUG
+    fprintf(stderr, "expr type: %s;\t", exprType.c_str());
+#endif
+    std::string exprStr = "";
+    if(expr->type() == "Number")
+    {
+        NumberExprAST *numberExpr = dynamic_cast<NumberExprAST *>(expr.get());
+        // std::unique_ptr<NumberExprAST> numberExpr;
+        // if (tmp != nullptr)
+        // {
+        //     expr.release();
+        //     numberExpr.reset(tmp);
+        // }
+        double number = (numberExpr->getNumber());
+#ifdef DEBUG
+        fprintf(stderr, "number: %f\n", number);
+#endif
+
+        return std::to_string(number);
+    }
+    else if(expr->type() == "Variable")
+    {
+        VariableExprAST *variableExpr = dynamic_cast<VariableExprAST *>(expr.get());
+        std::string variable = (variableExpr->getVariable());
+#ifdef DEBUG
+        fprintf(stderr, "variable: %s\n", variable.c_str());
+#endif
+
+        return variable;
+    }
+    else if(expr->type() == "Call")
+    {
+        CallExprAST *callExpr = dynamic_cast<CallExprAST *>(expr.get());
+        std::string callee = (callExpr->getCallee());
+#ifdef DEBUG
+        fprintf(stderr, "call: %s\n", callee.c_str());
+#endif
+        std::vector<std::unique_ptr<ExprAST>> &args = callExpr->getArgs();
+
+        std::vector<std::string> argsStr;
+        for(long unsigned int i = 0; i < args.size(); ++i)
+        {
+            std::string strTmp = PrintExpression(args.at(i));  // std::unique_ptr<ExprAST>& exprTmp = args.at(i);
+            argsStr.push_back(strTmp);
+        }
+        callee += "(";
+        for(long unsigned int i = 0; i < argsStr.size() - 1; ++i)
+        {
+            callee += argsStr.at(i) + ", ";
+        }
+        callee += argsStr.back() + ")";
+        return callee;
+    }
+    else if(expr->type() == "Binary")
+    {
+        BinaryExprAST *binOp = dynamic_cast<BinaryExprAST *>(expr.get());
+        // std::unique_ptr<BinaryExprAST> binOp = std::make_unique<BinaryExprAST>(expr); // a old wrong method
+        char op = binOp->getOp();
+        std::string opStr(1, op);
+#ifdef DEBUG
+        fprintf(stderr, "op: %s\n", opStr.c_str());
+#endif
+
+        std::unique_ptr<ExprAST> &lhs = binOp->getLHS();
+        std::string lhsStr = PrintExpression(lhs);
+        std::unique_ptr<ExprAST> &rhs = binOp->getRHS();
+        std::string rhsStr = PrintExpression(rhs);
+
+        exprStr += "(" + lhsStr + opStr + rhsStr + ")";
+    }
+    else
+    {
+        exprStr = "unknown expression";
+    }
+    return exprStr;
+}
+
+void PrintFunction(std::unique_ptr<FunctionAST> &fun)
+{
+    if(fun == nullptr)
+    {
+        fprintf(stderr, "this is a nullptr.\n");
+    }
+    else
+    {
+        std::string funcNameStr = fun->getFuncName();
+        std::vector<std::string> funcArgsStr = fun->getFuncArgs();
+        std::unique_ptr<ExprAST> &expr = fun->getFuncBody();
+
+        fprintf(stderr, "funcName: %s\n", funcNameStr.c_str());
+        fprintf(stderr, "Args list:\n");
+        if(funcArgsStr.size() == 0)
+        {
+            fprintf(stderr, "\tempty args");
+        }
+        else
+        {
+            for(auto it = funcArgsStr.begin(); it != funcArgsStr.end(); ++it)
+            {
+                fprintf(stderr, "%s ", (*it).c_str());
+            }
+        }
+        fprintf(stderr, "\nFunc Body:\n");
+        // expr->printExpr();
+        std::string funcBodyStr = PrintExpression(expr);
+        fprintf(stderr, "\t%s\n", funcBodyStr.c_str());
+    }
+}
---
+++ instance_of_parseExprFromStr/src/parserASTLY.cpp
@@ -0,0 +1,174 @@
+#include "basic.hpp"
+#include "laxerASTLY.hpp"
+#include "parserASTLY.hpp"
+#include "string.h"
+
+//===----------------------------------------------------------------------===//
+// Parser
+//===----------------------------------------------------------------------===//
+
+std::unique_ptr<ExprAST> ParseExpressionForStr();
+
+/// numberexpr ::= number
+std::unique_ptr<ExprAST> ParseNumberExprForStr()
+{
+    auto Result = std::make_unique<NumberExprAST>(NumVal1);
+    getNextTokenForStr();  // consume the number
+    return Result;
+}
+
+/// parenexpr ::= '(' expression ')'
+std::unique_ptr<ExprAST> ParseParenExprForStr()
+{
+    getNextTokenForStr();  // eat (.
+    auto V = ParseExpressionForStr();
+    if(!V)
+        return nullptr;
+
+    if(CurTokForStr != ')')
+        return LogErrorForStr("expected ')'");
+    getNextTokenForStr();  // eat ).
+    return V;
+}
+
+/// identifierexpr
+///   ::= identifier
+///   ::= identifier '(' expression* ')'
+std::unique_ptr<ExprAST> ParseIdentifierExprForStr()
+{
+    std::string IdName = IdentifierStr1;
+
+    getNextTokenForStr();  // eat identifier.
+
+    if(CurTokForStr != '(')  // Simple variable ref.
+        return std::make_unique<VariableExprAST>(IdName);
+
+    // Call.
+    getNextTokenForStr();  // eat (
+    std::vector<std::unique_ptr<ExprAST>> Args;
+    if(CurTokForStr != ')')
+    {
+        while(true)
+        {
+            if(auto Arg = ParseExpressionForStr())
+                Args.push_back(std::move(Arg));
+            else
+                return nullptr;
+
+            if(CurTokForStr == ')')
+                break;
+
+            if(CurTokForStr != ',')
+                return LogErrorForStr("Expected ')' or ',' in argument list");
+            getNextTokenForStr();
+        }
+    }
+
+    // Eat the ')'.
+    getNextTokenForStr();
+
+    return std::make_unique<CallExprAST>(IdName, std::move(Args));
+}
+
+/// primary
+///   ::= identifierexpr
+///   ::= numberexpr
+///   ::= parenexpr
+std::unique_ptr<ExprAST> ParsePrimaryForStr()
+{
+    switch(CurTokForStr)
+    {
+        default:
+            return LogErrorForStr("unknown token when expecting an expression");
+        case tok_identifier_forstr:
+            return ParseIdentifierExprForStr();
+        case tok_number_forstr:
+            return ParseNumberExprForStr();
+        case '(':
+            return ParseParenExprForStr();
+    }
+}
+
+/// binoprhs
+///   ::= ('+' primary)*
+std::unique_ptr<ExprAST> ParseBinOpRHSForStr(int ExprPrec, std::unique_ptr<ExprAST> LHS)
+{
+    // If this is a binop, find its precedence.
+    while(true)
+    {
+        int TokPrec = GetTokPrecedenceForStr();
+
+        // If this is a binop that binds at least as tightly as the current binop,
+        // consume it, otherwise we are done.
+        if(TokPrec < ExprPrec)
+            return LHS;
+
+        // Okay, we know this is a binop.
+        int BinOp = CurTokForStr;
+        getNextTokenForStr();  // eat binop
+
+        // Parse the primary expression after the binary operator.
+        auto RHS = ParsePrimaryForStr();
+        if(!RHS)
+            return nullptr;
+
+        // If BinOp binds less tightly with RHS than the operator after RHS, let
+        // the pending operator take RHS as its LHS.
+        int NextPrec = GetTokPrecedenceForStr();
+        if(TokPrec < NextPrec)
+        {
+            RHS = ParseBinOpRHSForStr(TokPrec + 1, std::move(RHS));
+            if(!RHS)
+                return nullptr;
+        }
+
+        // Merge LHS/RHS.
+        LHS = std::make_unique<BinaryExprAST>(BinOp, std::move(LHS), std::move(RHS));
+    }
+}
+
+/// expression
+///   ::= primary binoprhs
+///
+std::unique_ptr<ExprAST> ParseExpressionForStr()
+{
+    auto LHS = ParsePrimaryForStr();
+    if(!LHS)
+        return nullptr;
+
+    return ParseBinOpRHSForStr(0, std::move(LHS));
+}
+
+//从文件读入到string里
+std::string readFileIntoString(char * filename)
+{
+    std::ifstream ifile(filename);
+    //将文件读入到ostringstream对象buf中
+    std::ostringstream buf;
+    char ch;
+    while(buf&&ifile.get(ch))
+    buf.put(ch);
+    //返回与流对象buf关联的字符串
+    return buf.str();
+}
+std::unique_ptr<ExprAST> ParseExpressionFromString()
+{
+    //清空filestring，flag归零
+	if(CurTokForStr == ';')
+	{
+		filestring.clear();
+	    filestring.shrink_to_fit();
+	    flag = 0;
+	}
+	
+	char * fn = (char*)"pythonAfter.txt";
+	filestring = readFileIntoString(fn);
+	
+	getNextTokenForStr();    //eat first element
+    
+	if (auto E = ParseExpressionForStr()){
+		std::unique_ptr<ExprAST> es = E->Clone();
+	    return es;
+	}
+    return nullptr;
+}
