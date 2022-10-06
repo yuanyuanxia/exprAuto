eb189c4bb40046e09b2b3136b006e56490a779f1
yhr <1158162970@qq.com>
2022-10-06 07:31:47 +0000

    update src/simplify.py.
    Signed-off-by: yhr <1158162970@qq.com>

--- src/simplify.py
+++ src/simplify.py
@@ -2,12 +2,6 @@ from sympy import *
 from os import path as os_path
 
 # Simplify expr
-def parse(t_str):
-    expr = sympify(t_str, locals=support_char())
-    expr = simplify(expr)
-    expr = expand(expr)
-    expr = cancel(expr)
-    return expr
 
 
 def support_char():
@@ -17,170 +11,140 @@ def support_char():
     # Returns a dictionary of extended characters for use by the locals function in simplif()
     return ns
 
-# Get ')'
-def get_next(expr, index):
-    j = index + 1
-    while j >= 0:
-        if expr[j] == '(':
-            break
-        j -= 1
-    return j
+
+def parse(t_str):
+    expr = sympify(t_str, locals=support_char())
+    expr = simplify(expr)
+    expr = expand(expr)
+    expr = cancel(expr)
+    return expr
 
 
-# Expand expr,x**n ----> x*x*...*x,x**a.b ----> pow(x,a.b)
-def new_expand(expr):
-    """
-    index_lst:[]    存放要替换的幂次单项式第一个字符所在的位置
-    nextop_dic:{}   存放幂次单项式下一个操作符所在的位置
-    max_op:x        多项式中最后一个运算符所在的位置
-    coefficient_dic:{}  存放幂次单项式的幂
-    content_dic:{}  存放幂次单项式替换后的内容
 
-    """
-    result = ""
-
-    # 将需要替换字符的索引加入列表
+def find_index(expr):
     index_lst = []
     for i in range(len(expr)):
         if expr[i] == '*' and expr[i + 1] == '*':
-            index_lst.append(i - 1)
-
-    if index_lst:
-        operator_lst = ['+', '-', '*', '/']
-
-        nextop_dic = {}
-        # 获取幂次式下一个操作符所在的位置
-        for k in index_lst:
-            nextop_dic[k] = None
-            # 如果系数带括号
-            if expr[k + 3] == '(':
-                right_bracket = 0
-                # 先找到')'所在的位置
-                for p in range(k + 4, len(expr)):
-                    if expr[p] == ')':
-                        right_bracket = p
-                        break
-                # 从')'往后开始找下一个运算符
-                for q in range(right_bracket + 1, len(expr)):
-                    if expr[q] in operator_lst:
-                        nextop_dic[k] = q
-                        break
-            else:
-                for i in range(k + 3, len(expr)):
-                    if expr[i] in operator_lst:
-                        nextop_dic[k] = i
-                        break
+            index_lst.append(i)
+    return index_lst
 
 
-        # 获取最后一个运算符所在的位置
-        max_op = 0
+def find_base(expr, i):
+    op_lst = ['+', '-', '*', '/']
+    if expr[i - 1] == ')':
         flag = 0
-        while flag < len(expr):
-            if expr[flag] in operator_lst:
-                if expr[flag] == '*':
-                    if expr[flag + 1] == '*':  # 如果是**,直接跳到**后的第一个数字
-                        flag += 3
-                        continue
-                    else:
-                        max_op = flag
-                else:
-                    max_op = flag
-            flag += 1
-
-        # 如果max_op为0只有幂次
-        # 如果要替换的索引位置大于max_op,直接获取到末尾
-
-        # print("max_op:", max_op)
-
-        coefficient_dic = {}
-        for index in index_lst:
-            # max_op == 0说明只有一个幂次
-            # index > max_op说明在最后
-
-            # is_one用来判断表达式是否只是一个幂次表达式
-            # 当max_op==0且index==0时就会冲突
-            is_one = True
-            if index == 0:
-                for i in range(index + 3, len(expr)):
-                    if expr[i] in operator_lst:
-                        is_one = False
-                if is_one:
-                    coefficient = expr[index + 3:]
-                    coefficient_dic[index] = coefficient
-                else:
-                    for i in range(index + 3, len(expr)):
-                        if expr[i] in operator_lst:
-                            coefficient = expr[index + 3:i]
-                            coefficient_dic[index] = coefficient
-                            break
-            elif index > max_op:  # 如果大于最大运算符所在的位置,后边的都加上
-                coefficient = expr[index + 3:]
-                coefficient_dic[index] = coefficient
-            else:
-                # 如果幂次带括号
-                if expr[index + 3] == '(':
-                    for i in range(index + 3, len(expr)):
-                        if expr[i] == ')':
-                            coefficient = expr[index + 3:i + 1]
-                            coefficient_dic[index] = coefficient
-                            break
-                else:
-                    # 幂次不带括号
-                    for i in range(index + 3, len(expr)):
-                        if expr[i] in operator_lst:
-                            coefficient = expr[index + 3:i]
-                            coefficient_dic[index] = coefficient
-                            break
-
-
-        # 遍历字符的索引,获取要替换后的内容
-        content_dic = {}
-        for i in index_lst:
-            new = ""  # 存放替换的内容
-            if expr[i] == ')':
-                j = get_next(expr, i)
-                char = expr[j:i + 1]
-
-            else:
-                char = expr[i]
-
-            if ('.' in coefficient_dic[i]) or ('(' in coefficient_dic[i]):  # 幂次为非整数
-                new = "pow(" + char + "," + coefficient_dic[i] + ")"
-            else:  # 幂次为整数
-                new += char
-                for j in range(int(coefficient_dic[i]) - 1):  # 判断要展开几次
-                    new += "*"
-                    new += char
-            content_dic[i] = "(" + new + ")"
-
-        # 进行替换,生成表达式
-        dic_keys = list(content_dic.keys())
-
-        # 如果第一个要替换的内容带括号
-        if expr[dic_keys[0]] == ')':
-            j = get_next(expr, dic_keys[0])
-            result += expr[:j]
-        else:
-            result += expr[:dic_keys[0]]  # 先加上第一个要替换内容前面的内容
-
-        for i in range(len(dic_keys)):
-            if i == len(dic_keys) - 1:  # 如果是最后一个要替换的内容
-                result += content_dic[dic_keys[i]]  # 先加上要替换的内容
-                if nextop_dic:  # 如果有nextop_dic 说明是多项式
-                    if nextop_dic[dic_keys[i]]:  # 如果该单项式不是多项式中的最后一个
-                        result += expr[nextop_dic[dic_keys[i]]:]
-            else:
-                result += content_dic[dic_keys[i]]  # 先加上要替换的内容
-                if expr[dic_keys[i + 1]] == ')':
-                    j = get_next(expr, dic_keys[i + 1])
-                    result += expr[nextop_dic[dic_keys[i]]:j]
-                else:
-                    result += expr[nextop_dic[dic_keys[i]]:dic_keys[i + 1]]  # 再加上原来两个幂次单项式之间夹的内容
-        return result
+        j = i - 2
+        while j >= 0:
+            if expr[j] == ')':
+                flag += 1
+            if expr[j] == '(' and flag == 0:
+                break
+            if expr[j] == '(':
+                flag -= 1
+            j -= 1
+        return [expr[j:i], j]
     else:
-        return expr
+        j = i - 2
+        while j >= 0:
+            if expr[j] in op_lst or expr[j] == '(':
+                break
+            j -= 1
+        return [expr[j + 1:i], j + 1]
 
 
+def find_exp(expr, i):
+    op_lst = ['+', '-', '*', '/']
+    if expr[i + 2] == '(':
+        flag = 0
+        j = i + 3
+        while j <= len(expr) - 1:
+            if expr[j] == '(':
+                flag += 1
+            if expr[j] == ')' and flag == 0:
+                break
+            if expr[j] == ')':
+                flag -= 1
+            j += 1
+        print("j", j)
+        return [expr[i + 2:j + 1], j]
+    else:
+        j = i + 3
+        while j <= len(expr) - 1:
+            if expr[j] in op_lst or expr[j] == ')':
+                break
+            j += 1
+        return [expr[i + 2:j], j - 1]
+
+
+def gene_content(temp_dict):
+    '''
+    {6: [{'x': 5}, {'2': 8}], 11: [{'y': 10}, {'3': 13}]}
+    '''
+    content_dict = {}
+    for key in temp_dict.keys():
+        content = ''
+
+        if ('.' in temp_dict[key][1][0]) or ('(' in temp_dict[key][1][0]):
+            content = 'pow(' + temp_dict[key][0][0] + \
+                ',' + temp_dict[key][1][0] + ')'
+            content_dict[key] = content
+        else:
+            init = temp_dict[key][0][0]
+            for i in range(int(temp_dict[key][1][0])):
+                if i == int(temp_dict[key][1][0]) - 1:
+                    content += init
+                else:
+                    content += init
+                    content += '*'
+            content_dict[key] = '(' + content + ')'
+
+    return content_dict
+
+
+def gene_result(expr, temp_dict, content_dict):
+    final_expr = ''
+    temp_lst = []
+    key_index = 0
+    last_key = None
+    for key in temp_dict.keys():
+        if key_index == 0:
+            temp_lst.append(expr[:temp_dict[key][0][1]])
+            temp_lst.append(content_dict[key])
+            if len(temp_dict.keys()) == 1:
+                temp_lst.append(expr[temp_dict[key][1][1] + 1:])
+        elif key_index == len(temp_dict.keys()) - 1:
+            temp_lst.append(
+                expr[(temp_dict[last_key][1][1] + 1):temp_dict[key][0][1]])
+            temp_lst.append(content_dict[key])
+            temp_lst.append(expr[temp_dict[key][1][1] + 1:])
+        else:
+            temp_lst.append(
+                expr[(temp_dict[last_key][1][1] + 1):temp_dict[key][0][1]])
+            temp_lst.append(content_dict[key])
+        key_index += 1
+        last_key = key
+    for char in temp_lst:
+        final_expr += char
+    return final_expr
+
+# Expand expr,x**n ----> x*x*...*x,x**a.b ----> pow(x,a.b)
+
+
+def new_expand(expr):
+    index_lst = find_index(expr)
+    temp_dict = {}
+    if index_lst:
+        for index in index_lst:
+            base_dic = find_base(expr, index)
+            exp_dic = find_exp(expr, index)
+            temp_dict[index] = [base_dic, exp_dic]
+        print(temp_dict)
+        content_dict = gene_content(temp_dict)
+        # print(content_dict)
+        final_expr = gene_result(expr, temp_dict, content_dict)
+        return final_expr
+
+    return expr
 
 
 def process(i_filename="pythonBefore.txt", o_filename="pythonAfter.txt"):
@@ -191,10 +155,11 @@ def process(i_filename="pythonBefore.txt", o_filename="pythonAfter.txt"):
     with open(i_filename, "r", encoding="UTF-8") as f1, \
             open(o_filename, "w", encoding="UTF-8") as f2:
         input_str = f1.read().strip()
-        # print("process: input_str " + input_str)
+        print("process: input_str " + input_str)
         out_str = str(parse(input_str))
-        # print("process: out_str1 " + out_str)
+        print("process: out_str1 " + out_str)
         out_str = new_expand(out_str)
-        # print("process: out_str2 " + out_str)
+        print("process: out_str2 " + out_str)
         f2.write(out_str)
     return 1
+
