f5673c6d5c4ccf3d7ddbeba41566b8d47c123daf
jlg <jlg@mathlib.com>
2022-10-18 20:45:36 +0800

    Add the body of geneHerbieCode

--- include/geneCode.hpp
+++ include/geneCode.hpp
@@ -2,6 +2,16 @@
 #define _GENECODE
 
 #include "basic.hpp"
+#include<stdio.h>
+#include <fstream>
+#include<iostream>
+#include<vector>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::vector;
+using std::endl;
+using std::string;
 
 vector<string> getVariablesFromExpr(const ast_ptr &expr);
 
@@ -11,7 +21,7 @@ void geneCode(string exprStr, vector<string> vars);
 
 void geneOriginCode(string exprStr);
 
-void geneHerbieCode(string exprStr);
+void geneHerbieCode(string exprstr, vector<string> cs, string exprname, float v[], float u[]);
 
 void geneDaisyCode(string exprStr);
 
